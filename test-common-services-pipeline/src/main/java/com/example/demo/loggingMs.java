4fd16474a243b50c8eaf0cd4d798b1012c1fd510
Administrator <admin@example.com>
2023-12-13 11:50:26 +0530

    jw

--- test-common-services-pipeline/src/main/java/com/example/demo/loggingMs.java
+++ test-common-services-pipeline/src/main/java/com/example/demo/loggingMs.java
@@ -5,6 +5,7 @@ import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -24,44 +25,43 @@ public class loggingMs {
 
 	@Value("${jenkinsurl}")
 	private String jenkinsurl;
-	
+
 	@Value("${username}")
 	private String username;
-	
+
 	@Value("${password}")
 	private String password;
-	
+
 	@Value("${jenkinstoken}")
 	private String token;
-	
+
 	boolean responsecode = false;
+
+	RestTemplate restTemplate = new RestTemplate();
 
 	@PostMapping("/deploy-efk")
 	public String triggerPipeline(JsonNode body) throws JsonMappingException, JsonProcessingException {
-		
+
 		String authStr = username + ":" + password;
 		String base64Creds = Base64.getEncoder().encodeToString(authStr.getBytes());
 
-		RestTemplate resttemplate = new RestTemplate();
-		
 		HttpHeaders headers = new HttpHeaders();
-		
+
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		headers.setBasicAuth(base64Creds);
 		MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
-
 
 		requestBody.add("Namespace", "efk");
 		requestBody.add("Microservices", "Yes");
 		requestBody.add("Tools", "Yes");
 		requestBody.add("gitlab_ms_branch", "efk-logging");
 		requestBody.add("gitlab_tool_branch", "efk");
-		
 
 		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
-		String Jenkinsurl = jenkinsurl + "/job/cluster_microservices1" +  "/buildWithParameters?token=" + token;
+		String Jenkinsurl = jenkinsurl + "/job/cluster_microservices1" + "/buildWithParameters?token=" + token;
 		System.out.println(Jenkinsurl);
-		ResponseEntity<JsonNode> response = resttemplate.postForEntity(Jenkinsurl,new HttpEntity<>(requestBody, headers), JsonNode.class);
+		ResponseEntity<JsonNode> response = restTemplate.postForEntity(Jenkinsurl,
+				new HttpEntity<>(requestBody, headers), JsonNode.class);
 		responsecode = response.getStatusCode().is2xxSuccessful();
 		if (responsecode) {
 			System.out.println("Job triggered successfully");
@@ -73,5 +73,18 @@ public class loggingMs {
 			return "The job  has not been triggered";
 		}
 
+	}
+
+	@PostMapping("/pushLog")
+	public String postLog() {
+		String gateWayUrl = "http://aa7eabfa05ed24fc3ad6d2c4007e805c-1204085443.us-east-1.elb.amazonaws.com";
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("host", "efk.example.com");
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		String requestBody = "{\"message\":\"Sample Log\"}";
+		HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+		ResponseEntity<String> responseEntity = restTemplate.exchange(gateWayUrl + "/efk/pushLog/", HttpMethod.POST,
+				requestEntity, String.class);
+		return responseEntity.getBody();
 	}
 }
