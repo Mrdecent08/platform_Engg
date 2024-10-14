cb9b7d84338b4904fdad245e8454967a801a4696
Administrator <admin@example.com>
2024-10-14 14:11:41 +0530

    update

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:11
+
+EXPOSE 8080
+
+ADD target/s3-policies-0.0.1-SNAPSHOT.jar s3-policies-0.0.1-SNAPSHOT.jar 
+
+ENTRYPOINT [ "java","-jar","s3-policies-0.0.1-SNAPSHOT.jar"]
+
+
---
+++ src/main/java/com/example/demo/awsDynamicVolume.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class awsDynamicVolume {
+
+	public static void main(String[] args) {
+		SpringApplication.run(awsDynamicVolume.class, args);
+	}
+
+}
